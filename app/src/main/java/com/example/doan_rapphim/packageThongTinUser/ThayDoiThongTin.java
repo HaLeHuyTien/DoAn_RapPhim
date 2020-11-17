5b8632cc0e31e9ecfd67bab35576291d16304602
Quyet Ngo <nguynhuan123@gmail.com>
2020-11-17 10:19:35 +0700

    TT

--- app/src/main/java/com/example/doan_rapphim/packageThongTinUser/ThayDoiThongTin.java
+++ app/src/main/java/com/example/doan_rapphim/packageThongTinUser/ThayDoiThongTin.java
@@ -3,14 +3,30 @@ package com.example.doan_rapphim.packageThongTinUser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.doan_rapphim.R;
 
-public class ThayDoiThongTin extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ThayDoiThongTin extends AppCompatActivity {
+    private Spinner spnTp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thay_doi_thong_tin);
+    }
+
+    public void chonThang() {
+        List<String> list = new ArrayList<>();
+        for(int i = 1;i<13;i++) {
+            list.add("T "+ i);
+
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spnTp.setAdapter(adapter);
     }
 }

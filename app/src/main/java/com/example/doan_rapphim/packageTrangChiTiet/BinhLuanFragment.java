2bd760a1831f357546bd54c11111ed63c23fd4c2
HaLeHuyTien <71798010+HaLeHuyTien@users.noreply.github.com>
2020-12-10 13:55:31 +0700

    Thêm ApiWeb: Đăng Bình Luận

--- app/src/main/java/com/example/doan_rapphim/packageTrangChiTiet/BinhLuanFragment.java
+++ app/src/main/java/com/example/doan_rapphim/packageTrangChiTiet/BinhLuanFragment.java
@@ -1,7 +1,6 @@
 package com.example.doan_rapphim.packageTrangChiTiet;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -9,19 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresPermission;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.doan_rapphim.R;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,21 +31,29 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedList;
-import java.util.PrimitiveIterator;
 
 public class BinhLuanFragment extends Fragment {
 
 
     private  String jsonURLBL;
     private  String value= "http://0306181355.pixelcent.com/Cinema/BinhLuanTheoPhim.php?IDPhim=";
+    private  String jsonURLDangBL;
+    private  String valueDangBL = "http://0306181355.pixelcent.com/Cinema/DangBinhLuan.php?IDNguoiBinhLuan=";
 
-    private Button btn;
+    private EditText txtNDBinhLuan;
+    private Button btnBinhLuan;
+    private TextView txtThongBaoBL;
+
     //RecyclerView
     private final LinkedList<BinhLuan_Json> mWordList = new LinkedList<>();
     private RecyclerView recyclerView;
     private BinhLuanListAdapter adapter;
     private TrangChiTiet mtrangChiTiet;
+
 
     public static BinhLuanFragment getInstance(){
         BinhLuanFragment binhLuanFragment = new BinhLuanFragment();
@@ -64,12 +70,39 @@ public class BinhLuanFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.binhluan, container,false);
         recyclerView = view.findViewById(R.id.recycler_view_binh_luan);
-        btn = view.findViewById(R.id.btnDangBinhLuan);
+        btnBinhLuan = view.findViewById(R.id.btnDangBinhLuan);
+        txtNDBinhLuan = view.findViewById(R.id.editTextBinhLuan);
+        txtThongBaoBL = view.findViewById(R.id.txtThongBaoBL);
         mtrangChiTiet = (TrangChiTiet) getActivity();
         jsonURLBL = value + mtrangChiTiet.getIdPhim().toString();
 
+
+
         GetBinhLuan getBinhLuan = new GetBinhLuan();
         getBinhLuan.execute();
+
+        btnBinhLuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtNDBinhLuan.getText().toString().replace(" ","").equals(""))
+                {
+                    txtThongBaoBL.setText("Bạn phải nhập bình luận");
+                }
+                else{
+                    DateFormat df = new SimpleDateFormat("yyyy-MM-d");
+                    String date = df.format(Calendar.getInstance().getTime());
+                    DateFormat df2 = new SimpleDateFormat("HH:mm");
+                    String time = df2.format(Calendar.getInstance().getTime());
+                    jsonURLDangBL = valueDangBL + "1"+ "&IDPhim=" + mtrangChiTiet.getIdPhim() +"&Ngay="+date+"&Gio="+time+"&NoiDung=" + txtNDBinhLuan.getText().toString();
+                    DangBinhLuan dangBinhLuan = new DangBinhLuan();
+                    dangBinhLuan.execute();
+                    GetBinhLuan getBinhLuan1 = new GetBinhLuan();
+                    getBinhLuan1.execute();
+                    txtThongBaoBL.setText("");
+                    txtNDBinhLuan.setText("");
+                }
+            }
+        });
 
         return view;
 
@@ -88,7 +121,7 @@ public class BinhLuanFragment extends Fragment {
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         }catch (Exception e){
-            btn.setText("Lỗi");
+            btnBinhLuan.setText("Lỗi");
         }
 
     }
@@ -169,6 +202,55 @@ public class BinhLuanFragment extends Fragment {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private class DangBinhLuan extends AsyncTask<String, String, String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String current = "";
+
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+
+
+                try {
+                    url = new URL(jsonURLDangBL);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(in);
+
+                    int data = isr.read();
+                    while (data != -1) {
+                        current += (char) data;
+                        data = isr.read();
+                    }
+
+                    return current;
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            return current;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
         }
     }
 }

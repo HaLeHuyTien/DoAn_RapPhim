8a61f140f4cc20ab4999cd1b8c0fa8ae4248300c
DoanCongTrang <73584488+DoanCongTrang@users.noreply.github.com>
2020-12-19 10:51:07 +0700

    bo xung

--- app/src/main/java/com/example/doan_rapphim/packageTrangChiTiet/packageThanhToan/packageThanhToan/ThongTinSoDoGhe.java
+++
@@ -1,8 +0,0 @@
-package com.example.doan_rapphim.packageTrangChiTiet.packageThanhToan.packageThanhToan;
-
-public class ThongTinSoDoGhe {
-   public static String  tenPhim;
-    public static  String suatChieu;
-    public static String tenHinh;
-
-}

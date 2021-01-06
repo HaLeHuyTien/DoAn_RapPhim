02988303a31093ae6f4f387c6db28e0d69cf682a
Quyet Ngo <nguynhuan123@gmail.com>
2021-01-06 10:05:44 +0700

    Spinner Địa chỉ

---
+++ app/src/main/java/com/example/doan_rapphim/packageDangKyDangNhap/packageDangNhap/packageThongTinUser/PhuongXa.java
@@ -0,0 +1,60 @@
+package com.example.doan_rapphim.packageDangKyDangNhap.packageDangNhap.packageThongTinUser;
+
+import androidx.annotation.NonNull;
+
+public class PhuongXa {
+
+
+    public PhuongXa(String PhuongXa, int IDThanhPhoTinh, int IDQuanHuyen) {
+        this.PhuongXa = PhuongXa;
+        this.IDQuanHuyen = IDQuanHuyen;
+        this.IDThanhPhoTinh = IDThanhPhoTinh;
+    }
+
+    public PhuongXa() {
+        this.PhuongXa = getPhuongXa();
+        this.IDQuanHuyen = getIDQuanHuyen();
+    }
+
+    public String getPhuongXa() {
+        return PhuongXa;
+    }
+
+    public void setPhuongXa(String phuongXa) {
+        PhuongXa = phuongXa;
+    }
+
+    public int getIDQuanHuyen() {
+        return IDQuanHuyen;
+    }
+
+    public void setIDQuanHuyen(int IDQuanHuyen) {
+        this.IDQuanHuyen = IDQuanHuyen;
+    }
+
+    public int getIDThanhPhoTinh() {
+        return IDThanhPhoTinh;
+    }
+
+    public void setIDThanhPhoTinh(int IDThanhPhoTinh) {
+        this.IDThanhPhoTinh = IDThanhPhoTinh;
+    }
+
+
+
+    private String PhuongXa;
+    private int IDThanhPhoTinh;
+    private int IDQuanHuyen;
+
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return PhuongXa;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/doan_rapphim/packageDangKyDangNhap/packageDangNhap/packageThongTinUser/QuanHuyen.java
@@ -0,0 +1,44 @@
+package com.example.doan_rapphim.packageDangKyDangNhap.packageDangNhap.packageThongTinUser;
+
+import androidx.annotation.NonNull;
+
+public class QuanHuyen {
+
+
+
+
+
+
+    private String QuanHuyen;
+    private int IDThanhPhoTinh;
+
+    public QuanHuyen(String quanHuyen,int IDThanhPhoTinh) {
+        this.QuanHuyen = quanHuyen;
+        this.IDThanhPhoTinh = IDThanhPhoTinh;
+
+    }
+
+
+
+    public String getQuanHuyen() {
+        return QuanHuyen;
+    }
+
+    public void setQuanHuyen(String quanHuyen) {
+        QuanHuyen = quanHuyen;
+    }
+
+    public int getIDThanhPhoTinh() {
+        return IDThanhPhoTinh;
+    }
+
+    public void setIDThanhPhoTinh(int IDThanhPhoTinh) {
+        this.IDThanhPhoTinh = IDThanhPhoTinh;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return QuanHuyen;
+    }
+}

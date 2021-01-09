c9b9e0fd0cf6262625eb3e2a6c5feed01271900e
Quyet Ngo <nguynhuan123@gmail.com>
2021-01-09 15:06:16 +0700

    TongTienTheoNam(GD)

--- app/src/main/java/com/example/doan_rapphim/packageDangKyDangNhap/packageDangNhap/packageThongTinUser/GiaoDichFragment.java
+++ app/src/main/java/com/example/doan_rapphim/packageDangKyDangNhap/packageDangNhap/packageThongTinUser/GiaoDichFragment.java
@@ -1,6 +1,7 @@
 package com.example.doan_rapphim.packageDangKyDangNhap.packageDangNhap.packageThongTinUser;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,7 +17,20 @@ import android.widget.TextView;
 import com.example.doan_rapphim.R;
 import com.example.doan_rapphim.packageDangKyDangNhap.packageDangNhap.IDUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 /**
@@ -25,18 +39,23 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class GiaoDichFragment extends Fragment {
-    private TextView txtHVT;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
     private Spinner spnThang;
     private Button btnLichSuDV;
+    private TextView txtHVT;
+    private TextView txtTTNam;
+    private TextView txtTTThang;
 
+    private String URLTongTien;
+    private String URLValue = "http://0306181355.pixelcent.com/Cinema/TongTienTheoNam.php?IDKhachHang=";
     public GiaoDichFragment() {
         // Required empty public constructor
     }
@@ -75,7 +94,10 @@ public class GiaoDichFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_giao_dich, container, false);
         spnThang = view.findViewById(R.id.spinnerThang);
         txtHVT = view.findViewById(R.id.txtHVTGD);
+        txtTTNam = view.findViewById(R.id.txtTTNam);
+        txtTTThang = view.findViewById(R.id.txtTTThang);
         btnLichSuDV = view.findViewById(R.id.btnLichSuDatVe);
+
 
         btnLichSuDV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +107,9 @@ public class GiaoDichFragment extends Fragment {
         });
 
         chonThang();
+
+        GetTongTienTheoNam getTongTienTheoNam = new GetTongTienTheoNam();
+        getTongTienTheoNam.execute();
         return view;
 
 
@@ -94,6 +119,7 @@ public class GiaoDichFragment extends Fragment {
         Intent intent = new Intent(getActivity(),DSLichSuDatVeActivity.class);
         startActivity(intent);
     }
+
     public void chonThang() {
         List<String> list = new ArrayList<>();
         for(int i = 0;i<13;i++) {
@@ -102,5 +128,69 @@ public class GiaoDichFragment extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
         spnThang.setAdapter(adapter);
+    }
+
+    private class GetTongTienTheoNam extends AsyncTask<String, String, String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String current = "";
+
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+
+
+                try {
+                    URLTongTien = URLValue + IDUser.idUser;
+                    url = new URL(URLTongTien);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(in);
+
+                    int data = isr.read();
+                    while (data != -1) {
+                        current += (char) data;
+                        data = isr.read();
+                    }
+
+                    return current;
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            return current;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.getJSONArray("DanhSach");
+
+                for(int i = 0; i<jsonArray.length(); i++) {
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    String TongTien = jsonObject1.getString("TongTien");
+
+                    txtTTNam.setText(TongTien + " VND");
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
